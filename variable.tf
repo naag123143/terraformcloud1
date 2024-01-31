c43640f884255d753de07205bbe842e69c61ac6d
naag123143 <yaragalla789@gmail.com>
2024-01-31 13:35:13 +0530

    1 st commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type - Instance Sizing"
+  type = string
+}
